d5a8eda3b48650bd4a3a8d7993cc0c5042c5c720
linuxgurugamer@gmail.com <linuxgurugamer@gmail.com>
2025-02-27 15:31:18 -0500

    Updated AssemblyVersion

--- CactEye2/AssemblyVersion.cs
+++ CactEye2/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.5.2.8")]
- [assembly: AssemblyFileVersion("1.5.2.8")]
+ [assembly: AssemblyVersion("1.5.3.1")]
+ [assembly: AssemblyFileVersion("1.5.3.1")]
