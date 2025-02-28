f6e167a575596bc1fd7ae6a3cb7d645c8627e894
linuxgurugamer@gmail.com <linuxgurugamer@gmail.com>
2025-02-28 12:43:16 -0500

    Patches to add the ModuleCargoPart to all the processors

--- CactEye2/AssemblyVersion.cs
+++ CactEye2/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.5.3.1")]
- [assembly: AssemblyFileVersion("1.5.3.1")]
+ [assembly: AssemblyVersion("1.5.4.0")]
+ [assembly: AssemblyFileVersion("1.5.4.0")]
